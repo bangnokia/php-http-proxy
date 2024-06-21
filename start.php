bcef77df679d1f667c08ac835ffa7e63fb008b2c
bangnokia <bangnokia@gmail.com>
2024-06-22 03:10:42 +0700

    support custom username and password

--- start.php
+++ start.php
@@ -18,31 +18,33 @@ $worker->name = 'php-http-proxy';
 
 // Emitted when data received from client.
 $worker->onMessage = function (TcpConnection $connection, string $buffer) use ($username, $password) {
-    if (strpos($buffer, 'Proxy-Authorization') !== false) {
-        // find the base64 encoded string from buffer
-        preg_match('/Proxy-Authorization: Basic (.*)\r\n/', $buffer, $matches);
-        $auth = base64_decode($matches[1]);
-        $auth = explode(':', $auth);
+    if ($username && $password) {
+        if (strpos($buffer, 'Proxy-Authorization') !== false) {
+            // find the base64 encoded string from buffer
+            preg_match('/Proxy-Authorization: Basic (.*)\r\n/', $buffer, $matches);
+            $auth = base64_decode($matches[1]);
+            $auth = explode(':', $auth);
 
-        if ($auth[0] !== $username || $auth[1] !== $password) {
+            if ($auth[0] !== $username || $auth[1] !== $password) {
+                $connection->close();
+                return;
+            }
+        } else {
             $connection->close();
             return;
         }
-    } else {
-        $connection->close();
+    }
+
+    [$method, $address] = explode(' ', $buffer);
+    $parsedUrl = parse_url($address);
+
+    if (!isset($parsedUrl['host'])) {
         return;
     }
 
-    list($method, $addr, $http_version) = explode(' ', $buffer);
-    $url_data = parse_url($addr);
+    $address = $parsedUrl['host'] . ":" . $parsedUrl['port'] ?? 80;
 
-    if (!isset($url_data['host'])) {
-        return;
-    }
-
-    $addr = !isset($url_data['port']) ? "{$url_data['host']}:80" : "{$url_data['host']}:{$url_data['port']}";
-
-    $remoteConnection = new AsyncTcpConnection("tcp://$addr");
+    $remoteConnection = new AsyncTcpConnection("tcp://$address");
 
     if ($method !== 'CONNECT') {
         $remoteConnection->send($buffer);
